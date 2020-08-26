07991251fa26a2a70637b0dbccd7fd1d64ad34f6
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-08-26 20:58:46 +0300

    Add addAuthor Mutation

--- server.js
+++ server.js
@@ -97,6 +97,21 @@ const RootMutationType = new GraphQLObjectType({
         return book;
       },
     },
+    addAuthor: {
+      type: AuthorType,
+      description: 'Add an Author',
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parent, args) => {
+        const author = {
+          id: authors.length + 1,
+          name: args.name,
+        };
+        authors.push(author);
+        return author;
+      },
+    },
   }),
 });
 
